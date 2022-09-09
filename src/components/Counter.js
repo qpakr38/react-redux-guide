1a93e1f24a0e01c34d552af0e18ae11d151a98f1
qpakr38 <zczc0819@gmail.com>
2022-09-09 14:35:50 +0900

    REFACTORY : Redux Toolkit 사용하기

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,23 +1,25 @@
 import classes from './Counter.module.css';
 import {useDispatch, useSelector} from "react-redux";
 
+import {counterActions} from "../store/index";
+
 const Counter = () => {
     const dispatch = useDispatch();
     const counter = useSelector(state => state.counter);
     const showCounter = useSelector(state => state.showCounter);
 
     const incrementHandler = () => {
-        dispatch({type: 'increment', value: 1})
+        dispatch(counterActions.increment());
     }
     const increaseHandler = () => {
-        dispatch({type: 'increase', value: 5})
+        dispatch(counterActions.increase(5))
     }
     const decrementHandler = () => {
-        dispatch({type: 'decrement'})
+        dispatch(counterActions.decrement());
     }
 
     const toggleCounterHandler = () => {
-        dispatch({type: 'toggle'})
+        dispatch(counterActions.toggleCounter());
     };
     return (
         <main className={classes.counter}>
--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,5 @@
 import {createStore} from "redux";
-import {createSlice} from '@reduxjs/toolkit'
+import {configureStore, createSlice} from '@reduxjs/toolkit'
 
 const initialState = {counter: 0, showCounter: true};
 const counterSlice = createSlice({
@@ -13,14 +13,16 @@ const counterSlice = createSlice({
             state.counter--;
         },
         increase(state, action) {
-            state.counter=state.counter+action.value;
+            state.counter=state.counter+action.payload;
         },
         toggleCounter(state) {
             state.showCounter=!state.showCounter;
         }
     }
 });
+const store = configureStore({
+    reducer: counterSlice.reducer
+});
 
-const store = createStore(counterSlice.reducer);
-
+export const counterActions=counterSlice.actions;
 export default store;

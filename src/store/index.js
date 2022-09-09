06354d4f90a6c1461d05c12a49f7cd6e3705920a
qpakr38 <zczc0819@gmail.com>
2022-09-09 14:53:13 +0900

    FEAT: 다중 Slice 등록하기

--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,9 @@
-import {createStore} from "redux";
 import {configureStore, createSlice} from '@reduxjs/toolkit'
 
-const initialState = {counter: 0, showCounter: true};
+const initialCounterState = {counter: 0, showCounter: true};
 const counterSlice = createSlice({
     name: 'counter',
-    initialState,
+    initialState: initialCounterState,
     reducers: {
         increment(state) {
             state.counter++;
@@ -13,16 +12,35 @@ const counterSlice = createSlice({
             state.counter--;
         },
         increase(state, action) {
-            state.counter=state.counter+action.payload;
+            state.counter = state.counter + action.payload;
         },
         toggleCounter(state) {
-            state.showCounter=!state.showCounter;
+            state.showCounter = !state.showCounter;
+        }
+    }
+});
+const initialAuthState = {
+    isAuthenticated: false
+};
+const authSlice = createSlice({
+    name: 'authentication',
+    initialState: initialAuthState,
+    reducers: {
+        login(state) {
+            state.isAuthenticated = true;
+        },
+        logout(state) {
+            state.isAuthenticated = false;
         }
     }
 });
 const store = configureStore({
-    reducer: counterSlice.reducer
+    reducer: {
+        counter: counterSlice.reducer,
+        auth: authSlice.reducer
+    }
 });
 
-export const counterActions=counterSlice.actions;
+export const counterActions = counterSlice.actions;
+export const authActions = authSlice.actions;
 export default store;
